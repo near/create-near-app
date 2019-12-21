13cff385c43b71d125b768ad2568b1c188ee161a
Vladimir Grichina <vgrichina@gmail.com>
2019-12-20 16:22:35 -0800

    Copy and enable ESLint config from near-shell

--- index.js
+++ index.js
@@ -6,66 +6,66 @@ ncp.limit = 16;
 
 const exitOnError = async function(promise) {
     try {
-      await promise;
+        await promise;
     } catch (e) {
-      console.log("Error: ", e);
-      process.exit(1);
+        console.log('Error: ', e);
+        process.exit(1);
     }
-}
+};
 
 const createProject = {
-  command: '$0 <projectDir>',
-  desc: 'create a new blank react project',
-  builder: (yargs) => yargs 
-    .option('projectDir', {
-      desc: 'project directory',
-      type: 'string',
-      required: true
-    }),
-  handler: (argv) => exitOnError(create_Project(argv))
+    command: '$0 <projectDir>',
+    desc: 'create a new blank react project',
+    builder: (yargs) => yargs 
+        .option('projectDir', {
+            desc: 'project directory',
+            type: 'string',
+            required: true
+        }),
+    handler: (argv) => exitOnError(create_Project(argv))
 };
 
 const create_Project = async function(options) {
-  // Need to wait for the copy to finish, otherwise next tasks do not find files.
-  const projectDir = options.projectDir;
-  let sourceDir = __dirname + "/blank_react_project";
-  if(options.vanilla){
-    sourceDir = __dirname + '/blank_project';
-  }
-  console.log(`Copying files to new project directory (${projectDir}) from template source (${sourceDir}).`);
-  const copyDirFn = () => {
-      return new Promise(resolve => {
-          ncp (sourceDir, options.projectDir, response => resolve(response));
-  })};
-  await copyDirFn();
-  let path = projectDir + "/package.json";
-  let index = projectDir.lastIndexOf("/");
-  let name = index > 0 
-            ? projectDir.slice(index+1, ) 
-            : projectDir
-  fs.readFile(path,function(err, data){
-    if (err) {
-      throw 'could not read file: ' + err;
+    // Need to wait for the copy to finish, otherwise next tasks do not find files.
+    const projectDir = options.projectDir;
+    let sourceDir = __dirname + '/blank_react_project';
+    if(options.vanilla){
+        sourceDir = __dirname + '/blank_project';
     }
-    let json = JSON.parse(data);
-    json["name"] = name;
-    fs.writeFile(path,JSON.stringify(json, null, 4),function(err) {
-      if (err) { 
-        throw "error writing file: " + err;
-      }else {
-        console.log("wrote successfully!");
-      }
-    })
-  })
-  console.log('Copying project files complete.');
+    console.log(`Copying files to new project directory (${projectDir}) from template source (${sourceDir}).`);
+    const copyDirFn = () => {
+        return new Promise(resolve => {
+            ncp (sourceDir, options.projectDir, response => resolve(response));
+        });};
+    await copyDirFn();
+    let path = projectDir + '/package.json';
+    let index = projectDir.lastIndexOf('/');
+    let name = index > 0 
+        ? projectDir.slice(index+1, ) 
+        : projectDir;
+    fs.readFile(path,function(err, data){
+        if (err) {
+            throw 'could not read file: ' + err;
+        }
+        let json = JSON.parse(data);
+        json['name'] = name;
+        fs.writeFile(path,JSON.stringify(json, null, 4),function(err) {
+            if (err) { 
+                throw 'error writing file: ' + err;
+            }else {
+                console.log('wrote successfully!');
+            }
+        });
+    });
+    console.log('Copying project files complete.');
 };
 
 yargs
-  .option('vanilla',{
-    desc: 'create blank plain JS project',
-    type: 'boolean',
-    default: false
-  })
-  .command(createProject)
-  .help()
-  .argv;
+    .option('vanilla',{
+        desc: 'create blank plain JS project',
+        type: 'boolean',
+        default: false
+    })
+    .command(createProject)
+    .help()
+    .argv;
