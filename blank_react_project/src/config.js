cb8ac8e03284d5617ef6b5e69be33fbbb59b63ff
Vladimir Grichina <vgrichina@gmail.com>
2019-12-23 14:30:51 -0800

    Update blank_react_project config
    Remove initialBalance value

--- blank_react_project/src/config.js
+++ blank_react_project/src/config.js
@@ -4,59 +4,54 @@
     
     function getConfig(env) {
         switch (env) {
-            case 'production':
-            case 'development':
-                return {
-                    networkId: 'default',
-                    nodeUrl: 'https://rpc.nearprotocol.com',
-                    contractName: CONTRACT_NAME,
-                    walletUrl: 'https://wallet.nearprotocol.com',
-                    initialBalance: 100000000,
-                };
-            case 'staging':
-                return {
-                    networkId: 'staging',
-                    nodeUrl: 'https://staging-rpc.nearprotocol.com/',
-                    contractName: CONTRACT_NAME,
-                    walletUrl: 'https://near-wallet-staging.onrender.com',
-                    initialBalance: 100000000,
-                };
-            case 'local':
-                return {
-                    networkId: 'local',
-                    nodeUrl: 'http://localhost:3030',
-                    keyPath: `${process.env.HOME}/.near/validator_key.json`,
-                    walletUrl: 'http://localhost:4000/wallet',
-                    contractName: CONTRACT_NAME,
-                    initialBalance: 100000000,
-                };
-            case 'test':
-                return {
-                    networkId: 'local',
-                    nodeUrl: 'http://localhost:3030',
-                    contractName: CONTRACT_NAME,
-                    masterAccount: 'test.near',
-                    initialBalance: 100000000,
-                };
-            case 'test-remote':
-            case 'ci':
-                return {
-                    networkId: 'shared-test',
-                    nodeUrl: 'http://shared-test.nearprotocol.com:3030',
-                    contractName: CONTRACT_NAME,
-                    masterAccount: 'test.near',
-                    initialBalance: 100000000,
-                };
-            case 'ci-staging':
-                return {
-                    networkId: 'shared-test-staging',
-                    nodeUrl: 'http://staging-shared-test.nearprotocol.com:3030',
-                    contractName: CONTRACT_NAME,
-                    masterAccount: 'test.near',
-                    initialBalance: 100000000,
-                };
-            default:
-                throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`);
+
+        case 'production':
+        case 'development':
+            return {
+                networkId: 'default',
+                nodeUrl: 'https://rpc.nearprotocol.com',
+                contractName: CONTRACT_NAME,
+                walletUrl: 'https://wallet.nearprotocol.com',
+            };
+        case 'staging':
+            return {
+                networkId: 'staging',
+                nodeUrl: 'https://staging-rpc.nearprotocol.com/',
+                contractName: CONTRACT_NAME,
+                walletUrl: 'https://near-wallet-staging.onrender.com',
+            };
+        case 'local':
+            return {
+                networkId: 'local',
+                nodeUrl: 'http://localhost:3030',
+                keyPath: `${process.env.HOME}/.near/validator_key.json`,
+                walletUrl: 'http://localhost:4000/wallet',
+                contractName: CONTRACT_NAME,
+            };
+        case 'test':
+            return {
+                networkId: 'local',
+                nodeUrl: 'http://localhost:3030',
+                contractName: CONTRACT_NAME,
+                masterAccount: 'test.near',
+            };
+        case 'test-remote':
+        case 'ci':
+            return {
+                networkId: 'shared-test',
+                nodeUrl: 'http://shared-test.nearprotocol.com:3030',
+                contractName: CONTRACT_NAME,
+                masterAccount: 'test.near',
+            };
+        case 'ci-staging':
+            return {
+                networkId: 'shared-test-staging',
+                nodeUrl: 'http://staging-shared-test.nearprotocol.com:3030',
+                contractName: CONTRACT_NAME,
+                masterAccount: 'test.near',
+            };
+        default:
+            throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`);
         }
     }
     
