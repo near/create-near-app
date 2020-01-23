a8bb6ce07ff82adc928301d37237e983322a782f
Jane <jane.degtiareva@gmail.com>
2020-01-23 13:40:21 -0800

    Update contract to be closer to the blank_project example

--- blank_rust_project/contract/src/lib.rs
+++ blank_rust_project/contract/src/lib.rs
@@ -1,25 +1,19 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use near_bindgen::{env, near_bindgen};
-use std::collections::HashMap;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct StatusMessage {
-    records: HashMap<String, String>,
-}
+pub struct Welcome {}
 
 #[near_bindgen]
-impl StatusMessage {
-    pub fn set_status(&mut self, message: String) {
-        let account_id = env::signer_account_id();
-        self.records.insert(account_id, message);
-    }
-
-    pub fn get_status(&self, account_id: String) -> Option<String> {
-        self.records.get(&account_id).cloned()
+impl Welcome {
+    pub fn welcome(&self, name: String) -> Option<String> {
+        env::log(b"log message");
+        return Some(format!("Hello {}", name));
     }
 }
 
