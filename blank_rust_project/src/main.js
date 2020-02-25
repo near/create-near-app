8dcf56db22219f0e7da5a9d410d44691d48002c0
Jane <jane.degtiareva@gmail.com>
2020-02-25 12:52:22 -0800

    Adding a send txn in example rust app.

--- blank_rust_project/src/main.js
+++ blank_rust_project/src/main.js
@@ -26,7 +26,7 @@ async function InitContract() {
         // View methods are read only. They don't modify the state, but usually return some value.
         viewMethods: ['welcome'],
         // Change methods can modify the state. But you don't receive the returned value when called.
-        changeMethods: [],
+        changeMethods: ['set_greeting'],
         // Sender is the account ID to initialize transactions.
         sender: window.accountId,
     });
@@ -62,7 +62,7 @@ function signedInFlow() {
     // Displaying the signed in flow container.
     document.getElementById('signed-in-flow').classList.remove('d-none');
 
-    window.contract.welcome({account_id:window.accountId}).then(response => document.getElementById('speech').innerText = response.text);
+    getGreeting();
 
     // Adding an event to a sign-out button.
     document.getElementById('sign-out-button').addEventListener('click', () => {
@@ -70,6 +70,20 @@ function signedInFlow() {
         // Forcing redirect.
         window.location.replace(window.location.origin + window.location.pathname);
     });
+
+    // Adding an event to a sign-out button.
+    document.getElementById('change-greeting').addEventListener('click', () => {
+        setGreeting();
+    });
+}
+
+async function setGreeting() {
+    await window.contract.set_greeting({message:'Howdy'});
+    getGreeting();
+}
+
+function getGreeting() {
+    window.contract.welcome({account_id:window.accountId}).then(response => document.getElementById('speech').innerText = response.text);
 }
 
 // Loads nearlib and this contract into window scope.
