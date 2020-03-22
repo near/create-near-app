29c3f1d968ddc5566b088c0bbf7b83e8711b282d
Mike Purvis <mikedotexe@gmail.com>
2020-03-22 14:24:03 -0700

    fix lint indentation issues in near-shell-check

--- scripts/near-shell-check.js
+++ scripts/near-shell-check.js
@@ -6,20 +6,20 @@ const fs = require('fs');
 const util = require('util');
 
 (async () => {
-  const oldDevDeployFile = 'neardev/dev-account';
-  const newDevDeployFile = 'neardev/dev-account.env';
+    const oldDevDeployFile = 'neardev/dev-account';
+    const newDevDeployFile = 'neardev/dev-account.env';
 
-  if (fs.existsSync(newDevDeployFile)) {
-    // user already has the latest near-shell
-    return;
-  }
+    if (fs.existsSync(newDevDeployFile)) {
+        // user already has the latest near-shell
+        return;
+    }
 
-  if (fs.existsSync(oldDevDeployFile)) {
-    // user has an outdated near-shell, create necessary file
-    const readFile = (filePath) => util.promisify(fs.readFile)(filePath, 'utf8');
-    const writeFile = (filePath, data) => util.promisify(fs.writeFile)(filePath, data);
-    // read and rewrite the old file into the new format
-    const fileData = await readFile(oldDevDeployFile);
-    await writeFile(newDevDeployFile, `CONTRACT_NAME=${fileData}`);
-  }
+    if (fs.existsSync(oldDevDeployFile)) {
+        // user has an outdated near-shell, create necessary file
+        const readFile = (filePath) => util.promisify(fs.readFile)(filePath, 'utf8');
+        const writeFile = (filePath, data) => util.promisify(fs.writeFile)(filePath, data);
+        // read and rewrite the old file into the new format
+        const fileData = await readFile(oldDevDeployFile);
+        await writeFile(newDevDeployFile, `CONTRACT_NAME=${fileData}`);
+    }
 })();
