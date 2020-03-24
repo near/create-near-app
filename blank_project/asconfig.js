7865248242a6ff51a30f8ee6cc8da7ff5b8448ef
Willem Wyndham <willem@cs.umd.edu>
2020-03-24 18:37:27 -0400

    Update to near-sdk-as

---
+++ blank_project/asconfig.js
@@ -0,0 +1,12 @@
+const compile = require("near-sdk-as/compiler").compile
+
+compile("assembly/main.ts", // input file
+        "out/main.wasm",    // output file
+        [
+        //   "-O1",          // Optional arguments
+        "--debug",
+        "--measure",         // Shows compiler runtime
+        "--validate"         // Validate the generated wasm module
+        ], {
+          verbose: true     // Output the cli args passed to asc
+        });
