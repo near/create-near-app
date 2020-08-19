e503870ae5539fff22dfc7810347f5f279063b96
luciotato <luciotato@gmail.com>
2020-08-19 03:53:04 -0300

    fix: jest main test missing files

---
+++ templates/vue/src/main.test.js
@@ -0,0 +1,15 @@
+beforeAll(async function () {
+  // NOTE: nearlib and nearConfig are made available by near-cli/test_environment
+  const near = await nearlib.connect(nearConfig)
+  window.accountId = nearConfig.contractName
+  window.contract = await near.loadContract(nearConfig.contractName, {
+    viewMethods: ['getGreeting'],
+    changeMethods: [],
+    sender: window.accountId
+  })
+})
+
+test('getGreeting', async () => {
+  const message = await window.contract.getGreeting({ accountId: window.accountId })
+  expect(message).toEqual('Hello')
+})
