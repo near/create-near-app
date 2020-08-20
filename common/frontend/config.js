8931217c882907f854f96b5cdf27c2703cc01288
luciotato <luciotato@gmail.com>
2020-08-20 18:16:25 -0300

    fix: extra space removed

--- common/frontend/config.js
+++ common/frontend/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || 'near-blank-project'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'near-blank-project'
 
 function getConfig(env) {
   switch (env) {
