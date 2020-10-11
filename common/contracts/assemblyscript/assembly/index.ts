210240f572a406d01d3360a1fa9c484ec3702cc8
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-10-11 15:48:46 -0400

    feat: disambiguate assemblyscript
    * Move assemblyscript smart contract into own subdirectory,
      disambiguating frontend and smart contract dependencies
    * Remove `packagejsons` workaround, which resulted in an almost double
      maintenance burden from Dependabot updates
    * Fix generated READMEs, since the Vue and Angular templates referred to
      nonexistent files
    * Add consistent `compile` and `test` scripts to each kind of contract,
      with `--debug` flag
    * Generate symlink to most-recently-compiled version to `out/main.wasm`,
      rather than copying

--- common/contracts/assemblyscript/assembly/index.ts
+++ common/contracts/assemblyscript/assembly/index.ts
@@ -12,9 +12,9 @@
  *
  */
 
-import { Context, logging, storage } from "near-sdk-as";
+import { Context, logging, storage } from 'near-sdk-as'
 
-const DEFAULT_MESSAGE = "Hello"
+const DEFAULT_MESSAGE = 'Hello'
 
 // Exported functions will be part of the public interface for your smart contract.
 // Feel free to extract behavior to non-exported functions!
@@ -23,18 +23,18 @@ export function getGreeting(accountId: string): string | null {
   // storage for simple contracts.
   // If you have something more complex, check out persistent collections:
   // https://docs.near.org/docs/roles/developer/contracts/assemblyscript#imports
-  return storage.get<string>(accountId, DEFAULT_MESSAGE);
+  return storage.get<string>(accountId, DEFAULT_MESSAGE)
 }
 
 export function setGreeting(message: string): void {
-  const account_id = Context.sender;
+  const account_id = Context.sender
 
   // Use logging.log to record logs permanently to the blockchain!
   logging.log(
     // String interpolation (`like ${this}`) is a work in progress:
     // https://github.com/AssemblyScript/assemblyscript/pull/1115
     'Saving greeting "' + message + '" for account "' + account_id + '"'
-  );
+  )
 
-  storage.set(account_id, message);
+  storage.set(account_id, message)
 }
--- common/contracts/rust/build.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs')
-
-shell.fatal = true // same as "set -e"
-
-shell.cd('contract')
-// Note: see flags in ./cargo/config
-shell.exec('cargo build --target wasm32-unknown-unknown --release')
-shell.mkdir('-p', '../out')
-shell.cp('./target/wasm32-unknown-unknown/release/greeter.wasm', '../out/main.wasm')
