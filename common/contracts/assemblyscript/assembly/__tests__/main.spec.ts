7f97cb0e74c48497a0b1fe2cd1ef7a24b50761c1
Serhii Volovyk <sergeyvolovyk@gmail.com>
2021-03-29 23:24:12 +0300

    lint fix by hand

--- common/contracts/assemblyscript/assembly/__tests__/main.spec.ts
+++ common/contracts/assemblyscript/assembly/__tests__/main.spec.ts
@@ -4,6 +4,6 @@ import { storage, Context } from 'near-sdk-as'
 describe('Greeting ', () => {
   it('should be set and read', () => {
     setGreeting('hello world')
-    const greeting = storage.get<string>(Context.sender)
+    storage.get<string>(Context.sender)
   })
 })
