41bc6223232cbd1143cd39f778315358ae7e0ee7
Serhii Volovyk <SergeyVolovyk@gmail.com>
2021-03-31 18:07:04 +0300

    HashMap replaced with LookupMap (#840)
    feat(rust): near-sdk-rs upgraded to v3.0.1

--- common/contracts/rust/src/lib.rs
+++ common/contracts/rust/src/lib.rs
@@ -13,19 +13,25 @@
 
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::wee_alloc;
-use near_sdk::{env, near_bindgen};
-use std::collections::HashMap;
+use near_sdk::{env, near_bindgen, setup_alloc};
+use near_sdk::collections::LookupMap;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+setup_alloc!();
 
 // Structs in Rust are similar to other languages, and may include impl keyword as shown below
 // Note: the names of the structs are not important when calling the smart contract, but the function names are
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Welcome {
-    records: HashMap<String, String>,
+    records: LookupMap<String, String>,
+}
+
+impl Default for Welcome {
+  fn default() -> Self {
+    Self {
+      records: LookupMap::new(b"a".to_vec()),
+    }
+  }
 }
 
 #[near_bindgen]
@@ -36,16 +42,16 @@ impl Welcome {
         // Use env::log to record logs permanently to the blockchain!
         env::log(format!("Saving greeting '{}' for account '{}'", message, account_id,).as_bytes());
 
-        self.records.insert(account_id, message);
+        self.records.insert(&account_id, &message);
     }
 
     // `match` is similar to `switch` in other languages; here we use it to default to "Hello" if
     // self.records.get(&account_id) is not yet defined.
     // Learn more: https://doc.rust-lang.org/book/ch06-02-match.html#matching-with-optiont
-    pub fn get_greeting(&self, account_id: String) -> &str {
+    pub fn get_greeting(&self, account_id: String) -> String {
         match self.records.get(&account_id) {
             Some(greeting) => greeting,
-            None => "Hello",
+            None => "Hello".to_string(),
         }
     }
 }
--- utils/rust-setup.js
+++ utils/rust-setup.js
@@ -1,87 +1,87 @@
-const chalk = require('chalk');
-const os = require('os');
-const readline = require('readline');
-const sh = require('shelljs');
+const chalk = require('chalk')
+const os = require('os')
+const readline = require('readline')
+const sh = require('shelljs')
 
 // script from https://rustup.rs/ with auto-accept flag "-y"
-const installRustupScript = "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y";
-const updatePath = 'source $HOME/.cargo/env';
-const addWasm32TargetScript = "rustup target add wasm32-unknown-unknown";
+const installRustupScript = 'curl --proto \'=https\' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y'
+const updatePath = 'source $HOME/.cargo/env'
+const addWasm32TargetScript = 'rustup target add wasm32-unknown-unknown'
 // We should update PATH in the same script because every new Bash scripts are executed in a separate shell
-const updatePathAndAddWasm32TargetScript = updatePath + ' && ' + addWasm32TargetScript;
+const updatePathAndAddWasm32TargetScript = updatePath + ' && ' + addWasm32TargetScript
 
 const isWindows = os.platform() === 'win32'
 
 function isRustupInstalled() {
-  console.log(chalk`Checking if {bold rustup} is installed...`);
-  const isInstalled = sh.exec('rustup --version &> /dev/null').code === 0;
-  console.log(chalk`{bold rustup} is`, isInstalled ? 'installed\n' : 'not installed\n');
-  return isInstalled;
+  console.log(chalk`Checking if {bold rustup} is installed...`)
+  const isInstalled = sh.exec('rustup --version &> /dev/null').code === 0
+  console.log(chalk`{bold rustup} is`, isInstalled ? 'installed\n' : 'not installed\n')
+  return isInstalled
 }
 
 function isWasmTargetAdded() {
-  console.log(chalk`Checking installed {bold Rust targets}..`);
-  const addedTargets = sh.exec('rustup target list --installed').stdout;
-  const isWasmTargetAdded = addedTargets.includes('wasm32-unknown-unknown');
-  console.log(chalk`{bold wasm32-unknown-unknown} target `, isWasmTargetAdded ? 'already added' : 'is not added');
-  return isWasmTargetAdded;
+  console.log(chalk`Checking installed {bold Rust targets}..`)
+  const addedTargets = sh.exec('rustup target list --installed').stdout
+  const isWasmTargetAdded = addedTargets.includes('wasm32-unknown-unknown')
+  console.log(chalk`{bold wasm32-unknown-unknown} target `, isWasmTargetAdded ? 'already added' : 'is not added')
+  return isWasmTargetAdded
 }
 
 function installRustup() {
-  console.log(chalk`Installing {bold rustup}...`);
-  sh.exec(installRustupScript);
+  console.log(chalk`Installing {bold rustup}...`)
+  sh.exec(installRustupScript)
 }
 
 function addWasm32Target() {
-  console.log(chalk`Adding {bold wasm32-unknown-unknown} target...`);
-  sh.exec(updatePathAndAddWasm32TargetScript);
+  console.log(chalk`Adding {bold wasm32-unknown-unknown} target...`)
+  sh.exec(updatePathAndAddWasm32TargetScript)
 }
 
 async function askYesNoQuestionAndRunFunction(question, functionToRun = null) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-  });
+  })
   try {
     for (let attempt = 0; attempt < 4; attempt++) {
       const answer = await new Promise((resolve) => {
         rl.question(question, (userInput) => {
           if (['y', 'Y', ''].includes(userInput)) {
-            if (functionToRun) functionToRun();
-            resolve(true);
+            if (functionToRun) functionToRun()
+            resolve(true)
           }
           if (userInput === 'n') {
-            resolve(false);
+            resolve(false)
           }
-          resolve(undefined);
-        });
-      });
+          resolve(undefined)
+        })
+      })
       if (answer !== undefined) {
-        return answer;
+        return answer
       }
     }
   } finally {
-    rl.close();
+    rl.close()
   }
-  return false;
+  return false
 }
 
-const installRustupDisclaimer = chalk`In order to work with {bold rust} smart contracts we recommend you install {bold rustup}, the Rust toolchain installer.`;
-const addWasm32TargetDisclaimer = chalk`To build Rust smart contracts you need to add {bold WebAssembly} compiler target to Rust toolchain.`;
+const installRustupDisclaimer = chalk`In order to work with {bold rust} smart contracts we recommend you install {bold rustup}, the Rust toolchain installer.`
+const addWasm32TargetDisclaimer = chalk`To build Rust smart contracts you need to add {bold WebAssembly} compiler target to Rust toolchain.`
 
 const installRustupQuestion = chalk`
 ${installRustupDisclaimer} We can run the following command to do it for you:
 
     {bold ${installRustupScript}}
 
-Continue with installation (Y/n)?: `;
+Continue with installation (Y/n)?: `
 
 const addWasm32TargetQuestion = chalk`
 ${addWasm32TargetDisclaimer} We can run the following command to do it for you:
 
     {bold ${addWasm32TargetScript}}
 
-Continue with installation (Y/n)?: `;
+Continue with installation (Y/n)?: `
 
 const rustupAndWasm32WindowsInstallationInstructions = chalk`
 ${installRustupDisclaimer}
@@ -95,27 +95,27 @@ Run the following command to do it:
 
     {bold ${addWasm32TargetScript}}
 
-Press {bold Enter} to continue project creation.`;
+Press {bold Enter} to continue project creation.`
 
 async function setupRustAndWasm32Target() {
   try {
     if (isWindows) {
-      await askYesNoQuestionAndRunFunction(rustupAndWasm32WindowsInstallationInstructions);
-      return false;
+      await askYesNoQuestionAndRunFunction(rustupAndWasm32WindowsInstallationInstructions)
+      return false
     }
-    let wasRustupInstalled = false;
+    let wasRustupInstalled = false
     if (!isRustupInstalled()) {
-      wasRustupInstalled = await askYesNoQuestionAndRunFunction(installRustupQuestion, installRustup);
+      wasRustupInstalled = await askYesNoQuestionAndRunFunction(installRustupQuestion, installRustup)
     }
     if (!isWasmTargetAdded()) {
-      await askYesNoQuestionAndRunFunction(addWasm32TargetQuestion, addWasm32Target);
+      await askYesNoQuestionAndRunFunction(addWasm32TargetQuestion, addWasm32Target)
     }
-    return wasRustupInstalled;
+    return wasRustupInstalled
   } catch (e) {
-    console.log(chalk`Failed to run {bold rust} setup script`, e);
+    console.log(chalk`Failed to run {bold rust} setup script`, e)
   }
 }
 
 module.exports = {
   setupRustAndWasm32Target,
-};
+}
