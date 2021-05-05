788bc63c6c1154a4d4f8c796c605768ccaea60ea
Serhii Volovyk <SergeyVolovyk@gmail.com>
2021-05-05 22:16:16 +0300

    Tracking CI fix (#935)
    chore: redudant ci tracking prevented

--- tracking.js
+++ tracking.js
@@ -4,6 +4,11 @@ const chalk = require('chalk')
 const mixpanel = require('mixpanel').init(MIXPANEL_TOKEN)
 
 const track = async (frontendType, contractType) => {
+  // prevents logging from CI
+  if (process.env.IS_GITHUB_ACTION) { 
+    console.log('Mixpanel logging is skipped in CI env')
+    return
+  }
   try {
     console.log(chalk`
 NEAR collects basic information on the type of commands used to improve developer experiences. All data are anonymous, therefore, no personal information that could identify you will be shared.        `)
