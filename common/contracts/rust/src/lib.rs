0cdbf5dbd31195636c1a17ecbba7bf437959ab3f
austinabell <austinabell8@gmail.com>
2021-07-23 15:55:32 -0400

    feat: Update rust contract to 4.0 pre release

--- common/contracts/rust/src/lib.rs
+++ common/contracts/rust/src/lib.rs
@@ -13,25 +13,23 @@
 
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, setup_alloc};
 use near_sdk::collections::LookupMap;
-
-setup_alloc!();
+use near_sdk::{env, log, near_bindgen, AccountId};
 
 // Structs in Rust are similar to other languages, and may include impl keyword as shown below
 // Note: the names of the structs are not important when calling the smart contract, but the function names are
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Welcome {
-    records: LookupMap<String, String>,
+    records: LookupMap<AccountId, String>,
 }
 
 impl Default for Welcome {
-  fn default() -> Self {
-    Self {
-      records: LookupMap::new(b"a".to_vec()),
+    fn default() -> Self {
+        Self {
+            records: LookupMap::new(b"a"),
+        }
     }
-  }
 }
 
 #[near_bindgen]
@@ -39,20 +37,14 @@ impl Welcome {
     pub fn set_greeting(&mut self, message: String) {
         let account_id = env::signer_account_id();
 
-        // Use env::log to record logs permanently to the blockchain!
-        env::log(format!("Saving greeting '{}' for account '{}'", message, account_id,).as_bytes());
+        // Use `env::log_str` or `near_sdk::log!` to record logs permanently to the blockchain!
+        log!("Saving greeting '{}' for account '{}'", message, account_id);
 
         self.records.insert(&account_id, &message);
     }
 
-    // `match` is similar to `switch` in other languages; here we use it to default to "Hello" if
-    // self.records.get(&account_id) is not yet defined.
-    // Learn more: https://doc.rust-lang.org/book/ch06-02-match.html#matching-with-optiont
-    pub fn get_greeting(&self, account_id: String) -> String {
-        match self.records.get(&account_id) {
-            Some(greeting) => greeting,
-            None => "Hello".to_string(),
-        }
+    pub fn get_greeting(&self, account_id: AccountId) -> String {
+        self.records.get(&account_id).unwrap_or("Hello".to_string())
     }
 }
 
@@ -70,52 +62,33 @@ impl Welcome {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::MockedBlockchain;
-    use near_sdk::{testing_env, VMContext};
-
-    // mock the context for testing, notice "signer_account_id" that was accessed above from env::
-    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-        VMContext {
-            current_account_id: "alice_near".to_string(),
-            signer_account_id: "bob_near".to_string(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: "carol_near".to_string(),
-            input,
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            is_view,
-            output_data_receivers: vec![],
-            epoch_height: 19,
-        }
-    }
+    use near_sdk::{test_utils::VMContextBuilder, testing_env};
 
     #[test]
     fn set_then_get_greeting() {
-        let context = get_context(vec![], false);
-        testing_env!(context);
         let mut contract = Welcome::default();
         contract.set_greeting("howdy".to_string());
+
+        // The testing environment is initialized by default, so can just retrieve using `env`,
+        // which is also available in tests.
         assert_eq!(
             "howdy".to_string(),
-            contract.get_greeting("bob_near".to_string())
+            contract.get_greeting(env::signer_account_id())
         );
     }
 
     #[test]
     fn get_default_greeting() {
-        let context = get_context(vec![], true);
-        testing_env!(context);
         let contract = Welcome::default();
+
+        // Here we create a custom context to indicate that the following transaction is read-only
+        let context = VMContextBuilder::default().is_view(true).build();
+        testing_env!(context);
+
         // this test did not call set_greeting so should return the default "Hello" greeting
         assert_eq!(
             "Hello".to_string(),
-            contract.get_greeting("francis.near".to_string())
+            contract.get_greeting("francis.near".parse().unwrap())
         );
     }
 }
