12316d850f0ab8a53078e55a27e823e1ec154071
MaxGraey <maxgraey@gmail.com>
2021-08-22 17:14:10 +0300

    simplify

--- common/contracts/assemblyscript/assembly/index.ts
+++ common/contracts/assemblyscript/assembly/index.ts
@@ -23,18 +23,12 @@ export function getGreeting(accountId: string): string | null {
   // storage for simple contracts.
   // If you have something more complex, check out persistent collections:
   // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+  return storage.get(accountId, DEFAULT_MESSAGE)
 }
 
 export function setGreeting(message: string): void {
-  const account_id = Context.sender
-
+  const accountId = Context.sender
   // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
-
-  storage.set(account_id, message)
+  logging.log(`Saving greeting "${message}" for account "${accountId}"`)
+  storage.set(accountId, message)
 }
