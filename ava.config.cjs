dfa713196d32a42b509590c86d95dfc228c286e8
Max <max.khrp@gmail.com>
2022-07-29 21:53:45 +0300

    - fix tests

--- ava.config.cjs
+++
@@ -1,8 +0,0 @@
-require("util").inspect.defaultOptions.depth = 5; // Increase AVA's printing depth
-
-module.exports = {
-  timeout: "300000",
-  files: ["test/*.ava.js"],
-  failWithoutAssertions: false,
-  extensions: ["js"],
-};
