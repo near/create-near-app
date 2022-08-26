720aed2afdac21f92e6a12050cab77d1b855a56e
gagdiez <gagdiez.c@gmail.com>
2022-08-26 08:24:54 +0200

    homogenized the deploy process

--- templates/contracts/js/deploy.sh
+++ templates/contracts/js/deploy.sh
@@ -4,13 +4,10 @@
 
 echo ">> Deploying contract"
 
-# Deploy the contract with an auto-generated development account
-rm -rf neardev
 # https://docs.near.org/tools/near-cli#near-dev-deploy
 near dev-deploy --wasmFile build/hello_near.wasm
 
 export $(cat ./neardev/dev-account.env)
 
+# js contracts always need to be initialized
 near call $CONTRACT_NAME init --accountId $CONTRACT_NAME --deposit 1
-
-echo $CONTRACT_NAME
