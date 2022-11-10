9c176ee7323d8124f8eb7abf63d78e3ea8537fe7
Boris Tsao <boris.tsao@gmail.com>
2022-11-09 16:50:09 -0800

    link in contracts/rust/src/lib.rs was deprecated. updated to latest link to near website

--- templates/contracts/rust/src/lib.rs
+++ templates/contracts/rust/src/lib.rs
@@ -2,7 +2,7 @@
  * Example smart contract written in RUST
  *
  * Learn more about writing NEAR smart contracts with Rust:
- * https://near-docs.io/develop/Contract
+ * https://near-docs.io/develop/welcome
  *
  */
 
