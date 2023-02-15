728cb9da236476ca8a317b839bd106408e11dc8f
gagdiez <gagdiez.c@gmail.com>
2023-02-15 16:33:11 +0100

    updated react frontend

--- templates/frontend/react/index.js
+++ templates/frontend/react/index.js
@@ -1,6 +1,5 @@
 // React
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 // NEAR
@@ -11,13 +10,14 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_NAME
 // When creating the wallet you can optionally ask to create an access key
 // Having the key enables to call non-payable methods without interrupting the user to sign
 const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS })
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 // Setup on page load
 window.onload = async () => {
   const isSignedIn = await wallet.startUp()
- 
-  ReactDOM.render(
-    <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />,
-    document.getElementById('root')
+
+  root.render(
+    <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />
   );
 }
