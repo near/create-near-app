666b8a4988b9074967d55973e4e093eebbfc4fc9
gagdiez <gagdiez.c@gmail.com>
2023-10-26 09:03:59 -0300

    removed config

--- templates/frontend/gateway/src/utils/config.ts
+++ templates/frontend/gateway/src/utils/config.ts
@@ -7,20 +7,6 @@ export const networks: Record<NetworkId, Network> = {
     nodeUrl: 'https://rpc.mainnet.near.org',
     walletUrl: 'https://wallet.near.org',
     helperUrl: 'https://helper.mainnet.near.org',
-    fastAuth: {
-      mpcRecoveryUrl: 'https://mpc-recovery-leader-mainnet-cg7nolnlpa-ue.a.run.app',
-      authHelperUrl: 'https://api.kitwallet.app',
-      accountIdSuffix: 'near',
-      firebase: {
-        apiKey: 'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
-        authDomain: 'near-fastauth-prod.firebaseapp.com',
-        projectId: 'near-fastauth-prod',
-        storageBucket: 'near-fastauth-prod.appspot.com',
-        messagingSenderId: '829449955812',
-        appId: '1:829449955812:web:532436aa35572be60abff1',
-        measurementId: 'G-T2PPJ8QRYY',
-      },
-    },
   },
   testnet: {
     networkId: 'testnet',
@@ -28,20 +14,6 @@ export const networks: Record<NetworkId, Network> = {
     nodeUrl: 'https://rpc.testnet.near.org',
     walletUrl: 'https://wallet.testnet.near.org',
     helperUrl: 'https://helper.testnet.near.org',
-    fastAuth: {
-      mpcRecoveryUrl: 'https://mpc-recovery-7tk2cmmtcq-ue.a.run.app',
-      authHelperUrl: 'https://testnet-api.kitwallet.app',
-      accountIdSuffix: 'testnet',
-      firebase: {
-        apiKey: 'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
-        authDomain: 'pagoda-oboarding-dev.firebaseapp.com',
-        projectId: 'pagoda-oboarding-dev',
-        storageBucket: 'pagoda-oboarding-dev.appspot.com',
-        messagingSenderId: '116526963563',
-        appId: '1:116526963563:web:053cb0c425bf514007ca2e',
-        measurementId: 'G-HF2NBGE60S',
-      },
-    },
   },
 };
 
--- templates/frontend/gateway/src/utils/types.ts
+++ templates/frontend/gateway/src/utils/types.ts
@@ -16,18 +16,4 @@ type ProductionNetwork = {
   nodeUrl: string;
   walletUrl: string;
   helperUrl: string;
-  fastAuth: {
-    mpcRecoveryUrl: string;
-    authHelperUrl: string; // TODO refactor: review by fastauth team
-    accountIdSuffix: string;
-    firebase: {
-      apiKey: string;
-      authDomain: string;
-      projectId: string;
-      storageBucket: string;
-      messagingSenderId: string;
-      appId: string;
-      measurementId: string;
-    };
-  };
 };
