dc115fbbb385f2faa978a25ee75e21fd335116a0
gagdiez <gagdiez.c@gmail.com>
2024-05-14 17:10:59 -0300

    fix: components

--- templates/frontend/components/next-page/src/components/vm.js
+++ templates/frontend/components/next-page/src/components/vm.js
@@ -14,12 +14,16 @@ export default function Component({ src }) {
     wallet && initNear && initNear({ networkId: NetworkId, selector: wallet.selector, config: { allowOtherContracts: true } });
   }, [wallet, initNear]);
 
+  const href = wallet.networkId === 'mainnet' ?
+    `https://near.social/mob.near/widget/WidgetSource?src=${src}` :
+    `https://test.near.social/eugenethedream/widget/WidgetSource?src=${src}`;
+
   return (
     <div>
       <EthersProviderContext.Provider value={ethersContext}>
         <Widget src={src} />
       </EthersProviderContext.Provider>
-      <p className="mt-4 small"> <span className="text-secondary">Source:</span> <a href={`https://near.social/mob.near/widget/WidgetSource?src=${src}`}> {src} </a> </p>
+      <p className="mt-4 small"> <span className="text-secondary">Source:</span> <a href={href}> {src} </a> </p>
     </div>
   );
 }
