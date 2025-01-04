a80ddd506f4bb085334761fcc9363e477720a495
Guillermo Alejandro Gallardo Diez <gagdiez@iR2.home>
2025-01-04 01:23:16 +0100

    feat: update contracts

--- templates/contracts/rs/tests/test_basics.rs
+++ templates/contracts/rs/tests/test_basics.rs
@@ -1,19 +1,18 @@
-use near_workspaces::types::NearToken;
 use serde_json::json;
-
-const FIVE_NEAR: NearToken = NearToken::from_near(5);
 
 #[tokio::test]
 async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
-    let sandbox = near_workspaces::sandbox().await?;
     let contract_wasm = near_workspaces::compile_project("./").await?;
 
-    let root = sandbox.root_account()?;
+    test_basics_on(&contract_wasm).await?;
+    Ok(())
+}
 
-    let user_account = root.create_subaccount("user").transact().await?.unwrap();
-    let contract_account = root.create_subaccount("contract").initial_balance(FIVE_NEAR).transact().await?.unwrap();
+async fn test_basics_on(contract_wasm: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let sandbox = near_workspaces::sandbox().await?;
+    let contract = sandbox.dev_deploy(contract_wasm).await?;
 
-    let contract = contract_account.deploy(&contract_wasm).await?.unwrap();
+    let user_account = sandbox.dev_create_account().await?;
 
     let outcome = user_account
         .call(contract.id(), "set_greeting")
@@ -22,12 +21,10 @@ async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>
         .await?;
     assert!(outcome.is_success());
 
-    let user_message_outcome: serde_json::Value = user_account
-        .view(contract.id(), "get_greeting")
-        .args_json(json!({}))
-        .await?
-        .json()?;
-    assert_eq!(user_message_outcome, "Hello World!");
+    let user_message_outcome = contract.view("get_greeting").args_json(json!({})).await?;
+    assert_eq!(user_message_outcome.json::<String>()?, "Hello World!");
 
     Ok(())
 }
+
+
