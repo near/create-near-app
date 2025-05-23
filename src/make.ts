8778b51e366277217a7ecd23c58e3a4980524005
garikbesson <garikbesson@gmail.com>
2025-05-23 12:45:05 +0100

    download rust contract template from cargo-near when it's possible

--- src/make.ts
+++ src/make.ts
@@ -1,9 +1,12 @@
-import { CreateContractParams, CreateGatewayParams } from './types';
-import * as show from './messages';
 import spawn from 'cross-spawn';
 import fs from 'fs';
 import { ncp } from 'ncp';
 import path from 'path';
+import { execSync } from 'child_process';
+import * as show from './messages';
+import { CreateContractParams, CreateGatewayParams } from './types';
+
+const fsp = fs.promises;
 
 export async function createProject({ contract, frontend, projectPath, templatesDir }: CreateContractParams & CreateGatewayParams): Promise<boolean> {
   if (contract !== 'none') {
@@ -16,11 +19,40 @@ export async function createProject({ contract, frontend, projectPath, templates
 }
 
 async function createContract({ contract, projectPath, templatesDir }: CreateContractParams) {
+  if (contract === 'rs') {
+    const repoUrl = 'https://github.com/near/cargo-near.git';
+    const folderInRepo = 'cargo-near/src/commands/new/new-project-template';
+    const targetDir = path.join(projectPath);
+
+    try {
+      fs.mkdirSync(targetDir, { recursive: true });
+      execSync('git init', { cwd: targetDir });
+      execSync(`git remote add origin ${repoUrl}`, { cwd: targetDir });
+      execSync('git config core.sparseCheckout true', { cwd: targetDir });
+      fs.writeFileSync(path.join(targetDir, '.git/info/sparse-checkout'), `${folderInRepo}\n`);
+      execSync('git pull origin main', { cwd: targetDir });
+
+      const extractedPath = path.join(targetDir, folderInRepo);
+      const finalPath = path.resolve(targetDir);
+
+      await fsp.cp(extractedPath, finalPath, { recursive: true, force: true });
+      await fsp.rename(path.join(finalPath, 'Cargo.template.toml'), path.join(finalPath, 'Cargo.toml'));
+      await fsp.rm(path.join(finalPath, '.git'), { recursive: true, force: true });
+      await fsp.rm(path.join(targetDir, folderInRepo.split('/')[0]), { recursive: true, force: true });
+    } catch (err) {
+      show.downloadTemplateFailed();
+      await createContractFromTemplate({ contract, projectPath, templatesDir });
+    }
+  } else {
+    await createContractFromTemplate({ contract, projectPath, templatesDir });
+  }
+}
+
+async function createContractFromTemplate({ contract, projectPath, templatesDir }: CreateContractParams) {
   // contract folder
   const sourceContractDir = path.resolve(templatesDir, 'contracts', contract);
   fs.mkdirSync(projectPath, { recursive: true });
   await copyDir(sourceContractDir, projectPath);
-
 }
 
 async function createGateway({ frontend, projectPath, templatesDir }: CreateGatewayParams) {
